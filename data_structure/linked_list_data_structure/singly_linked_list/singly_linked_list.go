334fa2c11b762cac7a0a35fbbc84662e0b11d21f
nikhilsharma050 <nkhlsharma050@gmail.com>
2021-04-26 22:21:27 +0530

    insert AtFrontorBack and deleteAtfrontorBack

--- data_structure/linked_list_data_structure/singly_linked_list/singly_linked_list.go
+++ data_structure/linked_list_data_structure/singly_linked_list/singly_linked_list.go
@@ -14,7 +14,7 @@ type SinglyLinkedList struct {
 	length int
 }
 
-func (singlyLinkedList *SinglyLinkedList) insert(val int) {
+func (singlyLinkedList *SinglyLinkedList) insertAtBack(val int) {
 	node := &Node{value: val}
 	if singlyLinkedList.head == nil {
 		singlyLinkedList.head = node
@@ -29,25 +29,71 @@ func (singlyLinkedList *SinglyLinkedList) insert(val int) {
 	return
 }
 
-func (singlyLinkedList *SinglyLinkedList) Traverse() {
+func (singlyLinkedList *SinglyLinkedList) insertAtFront(val int) {
+
+	node := &Node{value: val}
+
 	if singlyLinkedList.head == nil {
-		fmt.Println("Empty List")
-		return
+		singlyLinkedList.head = node
+	} else {
+		node.next = singlyLinkedList.head
+		singlyLinkedList.head = node
+	}
+	singlyLinkedList.length++
+
+}
+
+func (singlyLinkedList *SinglyLinkedList) deleteAtFront() error {
+
+	if singlyLinkedList.head == nil {
+		return fmt.Errorf("List is empty")
+	}
+	singlyLinkedList.head = singlyLinkedList.head.next
+	singlyLinkedList.length--
+	return nil
+}
+
+func (singlyLinkedList *SinglyLinkedList) deleteAtBack() error {
+	if singlyLinkedList.head == nil {
+		return fmt.Errorf("List is Empty")
+	}
+	current := singlyLinkedList.head
+	var prev *Node
+	for current.next != nil {
+		prev = current
+		current = current.next
+	}
+	if prev != nil {
+		prev.next = nil
+	} else {
+		singlyLinkedList.head = nil
+	}
+	singlyLinkedList.length--
+	return nil
+}
+
+func (singlyLinkedList *SinglyLinkedList)size(){
+	return singlyLinkedList.length
+}
+
+func (singlyLinkedList *SinglyLinkedList) traverse() error {
+	if singlyLinkedList.head == nil {
+		return fmt.Errorf("List is Empty")
 	}
 	current := singlyLinkedList.head
 	for current != nil {
 		fmt.Println(current.value)
 		current = current.next
 	}
-
+	return nil
 }
 
 func main() {
 
 	singlyLinkedLis := &SinglyLinkedList{}
-	singlyLinkedLis.insert(2)
-	singlyLinkedLis.insert(3)
-	singlyLinkedLis.insert(4)
-	singlyLinkedLis.Traverse()
+	singlyLinkedLis.insertAtBack(2)
+	singlyLinkedLis.insertAtBack(3)
+	singlyLinkedLis.insertAtBack(4)
+	singlyLinkedLis.traverse()
 
 }
