96a22a10a9990cab89922240c569c4e8f023eb20
NikhilSharma-NS <nkhlsharma050@gmail.com>
2024-03-05 22:05:10 +0530

    feat: concurrency

---
+++ learn_golang/ultimate_go/concurrency/example1/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
+
+func init() {
+
+	g := runtime.GOMAXPROCS(1)
+	fmt.Println(g)
+}
+
+func main() {
+	s := time.Now()
+	//fmt.Println(g)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		lowercase()
+		wg.Done()
+	}()
+
+	go func() {
+		uppercase()
+		wg.Done()
+	}()
+
+	fmt.Println("waiting finish")
+	wg.Wait()
+	fmt.Println("Main done")
+
+	diff := time.Now().Sub(s)
+	fmt.Println("Diff", diff)
+}
+
+func lowercase() {
+	for count := 0; count < 3; count++ {
+		for r := 'a'; r <= 'z'; r++ {
+			fmt.Printf("%c\t", r)
+		}
+		fmt.Println()
+	}
+}
+func uppercase() {
+	for count := 0; count < 3; count++ {
+		for r := 'A'; r <= 'Z'; r++ {
+			fmt.Printf("%c\t", r)
+		}
+		fmt.Println()
+	}
+}
