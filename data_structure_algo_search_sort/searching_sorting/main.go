a31225aef8eeef133475c935112177bff43284de
NikhilSharma-NS <nkhlsharma050@gmail.com>
2024-03-18 23:26:02 +0530

    feat: merge sort

--- data_structure_algo_search_sort/searching_sorting/main.go
+++ data_structure_algo_search_sort/searching_sorting/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"Gorepo/go_programing/data_structure_algo_search_sort/searching_sorting/merge_sort"
 	"Gorepo/go_programing/data_structure_algo_search_sort/searching_sorting/quick_sort"
 	"fmt"
 )
@@ -9,7 +10,10 @@ func main() {
 
 	quick_sorted_array := quick_sort.QuickSort([]int{6, 3, 9, 5, 2, 8})
 	fmt.Println("Input Array  Using Quick Sort::[6, 3, 9, 5, 2, 8]")
-
 	fmt.Println("Output Array Using Quick Sort:", quick_sorted_array)
+
+	merged_sorted_array := merge_sort.MergeSort([]int{6, 3, 9, 5, 2, 8})
+	fmt.Println("Input  Array Using Merge Sort::[6, 3, 9, 5, 2, 8]")
+	fmt.Println("Output Array Using Merge Sort", merged_sorted_array)
 
 }
--- data_structure_algo_search_sort/searching_sorting/merge_sort/merge_sort.go
+++ data_structure_algo_search_sort/searching_sorting/merge_sort/merge_sort.go
@@ -1,5 +1,50 @@
 package merge_sort
 
-func MergeSort() {
+func MergeSort(arr []int) []int {
+	divide(arr, 0, len(arr)-1)
+	return arr
+}
+
+func divide(arr []int, start_index int, end_index int) {
+	if start_index >= end_index {
+		return
+	}
+	mid_index := start_index + (end_index-start_index)/2
+
+	divide(arr, start_index, mid_index)
+	divide(arr, mid_index+1, end_index)
+	conquer(arr, start_index, mid_index, end_index)
+
+}
+
+func conquer(arr []int, start_index int, mid_index int, end_index int) {
+
+	merg_arr := []int{}
+	index1 := start_index
+	index2 := mid_index + 1
+
+	for index1 <= mid_index && index2 <= end_index {
+		if arr[index1] <= arr[index2] {
+			merg_arr = append(merg_arr, arr[index1])
+			index1++
+		} else {
+			merg_arr = append(merg_arr, arr[index2])
+			index2++
+
+		}
+	}
+
+	for index1 <= mid_index {
+		merg_arr = append(merg_arr, arr[index1])
+		index1++
+	}
+	for index2 <= end_index {
+		merg_arr = append(merg_arr, arr[index2])
+		index2++
+	}
+
+	for i, j := 0, start_index; i < len(merg_arr); i, j = i+1, j+1 {
+		arr[j] = merg_arr[i]
+	}
 
 }
